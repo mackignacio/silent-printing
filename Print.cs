848c3a949556940720a096e7dfde4c5cc2105cc9
Mack <mack_ignacio@yahoo.com>
2019-12-17 23:39:44 +0800

    Feat(header) - Add printing of Business Name

--- Print.cs
+++ Print.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using silent_printing.Properties;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Printing;
+using System.Drawing.Text;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +15,7 @@ using System.Windows.Forms;
 class Print
 {
     private int verticalSpacing = 0;
+    private int logoHeight = 0;
     public Print(string printer, string paper, int width, int height, string[] args)
     {
         verticalSpacing = height;
@@ -20,28 +24,70 @@ class Print
         {
             pd.PrinterSettings.PrinterName = printer;
             pd.DefaultPageSettings.PaperSize = new PaperSize(paper, width, height);
-            pd.PrintPage += PrintPage(args);
+            pd.PrintPage += PrintPage(args, width);
             pd.Print();
         }
     }
 
-    private PrintPageEventHandler PrintPage(string[] args)
+    private PrintPageEventHandler PrintPage(string[] args, int width)
     {
         return (object sender, PrintPageEventArgs e) =>
         {
-            Graphics g = e.Graphics;
-            Logo(args[0], g, new PointF(50, 0));
+            Graphics graphics = e.Graphics;
+            Logo(args[0], graphics, new PointF(50, 0));
+            Headers(args, width, graphics);
         };
     }
 
-    private void Logo(string imgURL, Graphics g, PointF point)
+    private void Logo(string imgURL, Graphics graphics, PointF point)
     {
         using (WebClient wc = new WebClient())
         {
             byte[] bytes = wc.DownloadData(imgURL);
             MemoryStream ms = new MemoryStream(bytes);
             Image img = Image.FromStream(ms);
-            g.DrawImage(img, point);
+            graphics.DrawImage(img, point);
+            logoHeight = img.Height;
+        }
+    }
+
+    private void Headers(string[] args, int width, Graphics graphics)
+    {
+        string businessName = args[1];
+        using (Font headerFont = GetCustomFont(Resources.HelveticaNeueBd, 12, FontStyle.Bold))
+            SetBusinessName(LimitTextWidth(businessName, 20), headerFont, width, graphics);
+    }
+
+    private void SetBusinessName(string[] text, Font font, int width, Graphics graphics)
+    {
+        graphics.DrawString(text[0], font, Brushes.Black, Position(width, text[0], font, VerticalSpacing(logoHeight + 25)));
+        if (text.Length > 1) graphics.DrawString(text[1], font, Brushes.Black, Position(width, text[1], font, VerticalSpacing(20)));
+    }
+
+    private string[] LimitTextWidth(string text, int length)
+    {
+        string strlist = "";
+        string excess = "";
+        string[] split = text.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+        split.ToList().ConvertAll(str =>
+        {
+            if (strlist.Length < length) strlist += str + " ";
+            else excess += str + " ";
+            return str;
+        });
+
+        return new string[] { strlist, excess };
+    }
+
+    private Font GetCustomFont(byte[] fontData, float size, FontStyle style)
+    {
+        using (PrivateFontCollection pfc = new PrivateFontCollection())
+        {
+            IntPtr fontPtr = Marshal.AllocCoTaskMem(fontData.Length);
+            Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+            pfc.AddMemoryFont(fontPtr, fontData.Length);
+            Marshal.FreeCoTaskMem(fontPtr);
+            return new Font(pfc.Families[0], size, style);
         }
     }
 
@@ -51,16 +97,20 @@ class Print
         return verticalSpacing;
     }
 
-    private int TextWidht(string text, Font font)
+    private PointF Position(int width, string text, Font font, int height)
+    {
+        return new PointF(Center(width, TextWidth(text, font)), height);
+    }
+
+    private int TextWidth(string text, Font font)
     {
         return TextRenderer.MeasureText(text, font).Width;
     }
 
-    private int Center(int widht, int textLength)
+    private float Center(int width, int textLength)
     {
-        int w = (widht / 2);
-        int tl = (textLength / 2);
-        return w - tl;
+        float margin = (float)(width / 2) - (float)(textLength / 2);
+        return margin;
     }
 }
 
