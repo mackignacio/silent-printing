7f1b0607078e824a2dda8b73ab6575c74ac84da3
Mack <mack_ignacio@yahoo.com>
2020-02-26 15:21:22 +0800

    Add command on args

--- Program.cs
+++ Program.cs
@@ -1,10 +1,51 @@
-﻿namespace silent_printing
+﻿
+namespace silent_printing
 {
     class Program
     {
         static void Main(string[] args)
         {
-            new Print("EPSON TM-T82II Receipt", "Roll Paper 58 x 297 mm", 297, 0, args);
+            string argBody = System.Web.HttpUtility
+                 .UrlDecode(args[0])
+                 .Replace("neutronpos:", "");
+            string command = argBody.Substring(0, 1);
+            string[] options = argBody.Substring(1)
+                .Trim(new char[] { '"' })
+                .Split(new string[] { "\" \"" }, System.StringSplitOptions.RemoveEmptyEntries);
+
+            CashDrawer cashDrawer = new CashDrawer();
+
+            if (command == "0" || command == "2")
+            {
+                cashDrawer.Open();
+            }
+
+            string printerName = ""; 
+
+            foreach (string printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                System.Diagnostics.Debug.WriteLine(printer);
+                printer.Contains("EPSON");
+
+                if (printer.Contains("EPSON") || printer.Contains("Receipt"))
+                {
+                    printerName = printer;
+                }
+
+                  
+            }
+
+            if (command == "1" || command == "2")
+            {
+                new Print(printerName, "Roll Paper 58 x 297 mm", 297, 0, options);
+            }
+
+            if (command == "3" || command == "4")
+            {
+                new CreditCard(printerName, "Roll Paper 58 x 297 mm", 297, 0, options);
+            }
+
         }
+
     }
 }
