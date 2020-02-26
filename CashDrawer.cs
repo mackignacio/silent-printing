57f2d0357334a37b375a146b49fd996006d1f5cd
Mack <mack_ignacio@yahoo.com>
2020-02-26 15:19:14 +0800

    Add CashDrawer class

---
+++ CashDrawer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO.Ports;
+
+class CashDrawer
+{
+    public void Open()
+    {
+        string[] ports = SerialPort.GetPortNames();
+        foreach (string port in ports)
+        {
+
+            SerialPort serialPort = new SerialPort();
+
+            if (serialPort is SerialPort)
+            {
+                serialPort.PortName = port;
+                serialPort.BaudRate = 9600;
+                try
+                {
+                    serialPort.Open();
+                    if (serialPort.IsOpen)
+                    {
+                        serialPort.Write("OPEN");
+                    }
+                    serialPort.Close();
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc);
+                }
+            }
+
+        }
+    }
+    
+}
+
